5ed751b771ed65d18fad3bc0b376543f33d50c2c
coinjoker <32432165+coinjoker@users.noreply.github.com>
2017-11-19 20:53:01 +0100

    Added token sale code

--- SmartContracts/cjtoken.sol
+++ SmartContracts/cjtoken.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.4.16;
 // https://github.com/ethereum/EIPs/issues/20
 
 contract ERC20 {
-    function totalSupply() constant returns (uint totalSupply);
-    function balanceOf(address _owner) constant returns (uint balance);
-    function transfer(address _to, uint _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) returns (bool success);
-    function approve(address _spender, uint _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint remaining);
+    function totalSupply() constant returns (uint256 totalSupply);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
     
     // These generate a public event on the blockchain that will notify clients
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 }
 
@@ -35,10 +35,11 @@ contract owned {
 
 contract CJTToken is ERC20, owned {
     // Public variables of the token
-    string public name = "Coin Joker Token";
-    string public symbol = "CJT";
+    string public name = "Media Network Token";
+    string public symbol = "MNT";
     uint8 public decimals = 18;
-    uint256 public totalSupply = 105 * 10**6 * 10**18;
+    uint256 public totalSupply = 0; // 105 * 10**6 * 10**18;
+    uint256 public maxSupply = 105 * 10**6 * 10**18;
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
@@ -53,10 +54,13 @@ contract CJTToken is ERC20, owned {
      * Gives ownership of all initial tokens to the Coin Joker Team. Sets ownership of contract
      */
     function CJTToken(
-        address cjTeam;
+        address cjTeam
     ) {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        if (cjTeam != 0) owner = cjTeam;
+        //balanceOf[msg.sender] = totalSupply;              // Give the creator all initial tokens
+        totalEthRaised = 0;
+        if (cjTeam != 0) {
+            owner = cjTeam;
+        }
     }
 
     /**
@@ -65,9 +69,10 @@ contract CJTToken is ERC20, owned {
     function _transfer(
         address _from, 
         address _to, 
-        uint _value
-    ) internal {
-        require(_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        uint256 _value
+    ) internal 
+    {
+        require(_to != 0x0);                               // Prevent transfer to 0x0 address
         require(balanceOf[_from] >= _value);                // Check if the sender has enough
         require(balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         balanceOf[_from] -= _value;                         // Subtract from the sender
@@ -83,10 +88,7 @@ contract CJTToken is ERC20, owned {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(
-        address _to, 
-        uint256 _value
-    ) {
+    function transfer(address _to, uint256 _value) returns (bool success) {
         _transfer(msg.sender, _to, _value);
     }
 
@@ -103,7 +105,8 @@ contract CJTToken is ERC20, owned {
         address _from, 
         address _to, 
         uint256 _value
-    ) returns (bool success) {
+    ) returns (bool success) 
+    {
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
@@ -121,7 +124,8 @@ contract CJTToken is ERC20, owned {
     function approve(
         address _spender, 
         uint256 _value
-    ) returns (bool success) {
+    ) returns (bool success)
+    {
         allowance[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
@@ -133,7 +137,7 @@ contract CJTToken is ERC20, owned {
      * @param _owner The owner of the account
      */
     function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
+        return balanceOf[_owner];
     }
 
     /**
@@ -145,14 +149,70 @@ contract CJTToken is ERC20, owned {
     function allowance(
         address _owner, 
         address _spender
-    ) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
+    ) constant returns (uint256 remaining)
+    {
+        return allowance[_owner][_spender];
     }
 
     /**
      * Get total supply of all tokens
      */
-    function totalSupply() constant returns (uint totalSupply) {
+    function totalSupply() constant returns (uint256 supply) {
         return totalSupply;
     }
+
+    // --------------------------------
+    // Token sale variables and methods
+    // --------------------------------
+
+    bool saleHasStarted;
+    bool saleHasEnded;
+    uint256 public saleEndBlock;
+    uint256 public saleStartBlock;
+    uint256 public maxEthToRaise = 7500 * 10**18;
+    uint256 public totalEthRaised;
+    uint256 public eth2mnt = 10000 * 10**18; // number of MNTs you get for 1 ETH
+
+    /* Issue new tokens - internal function */     
+    function _mintTokens (address _to, uint256 _amount) internal {             
+        if (balanceOf[_to] + _amount < balanceOf[_to]) {
+            throw;     // Check for overflows
+        }
+        if (totalSupply + _amount > maxSupply) {
+            throw;
+        }
+        totalSupply += _amount;                                      // Update total supply
+        balanceOf[_to] += _amount;                               // Set minted coins to target
+        Transfer(0x0, _to, _amount);                            // Create Transfer event from 0x
+    }
+
+
+    /* Users send ETH and enter the token sale*/  
+    function () payable {
+        if (msg.value == 0) {
+            throw;                                          // Throw if the value is 0  
+        }
+        if (saleHasEnded || block.number > saleEndBlock) {
+            throw;        // Throw if the token sale has ended     
+        }
+        if (!saleHasStarted) {                                                // Check if this is the first token sale transaction       
+            if (block.number >= saleStartBlock) {                             // Check if the token sale should start        
+            saleHasStarted = true;                                           // Set that the token sale has started         
+            } else {
+                throw;
+            }
+        }     
+     
+        if (maxEthToRaise > (totalEthRaised + msg.value)) {                 // Check if the user sent too much ETH         
+            totalEthRaised += msg.value;                                    // Add to total eth Raised
+            _mintTokens(msg.sender, msg.value * eth2mnt);
+        } else {                                                              // If user sent to much eth       
+            uint maxContribution = maxEthToRaise - totalEthRaised;            // Calculate maximum contribution       
+            totalEthRaised += maxContribution;  
+            _mintTokens(msg.sender, maxContribution * eth2mnt);
+            uint toReturn = msg.value - maxContribution;                       // Calculate how much should be returned       
+            saleHasEnded = true;
+            msg.sender.send(toReturn);                                  // Refund the balance that is over the cap          
+        }
+    } 
 }
